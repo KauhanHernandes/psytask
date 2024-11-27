7e45c098c8824b80d0d58f0f1e5743edfa382c95
KauhanHernandes <kauhanhernandes@gmail.com>
2024-11-27 20:10:42 -0300

    bd

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,14 +1,10 @@
 require('dotenv').config();
 const mysql = require('mysql2');
 
-const connection = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-});
+// Usando a URL de conexão
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
+// Conectar ao banco de dados
 connection.connect((err) => {
     if (err) {
         console.error('Erro ao conectar ao banco de dados:', err.message);
